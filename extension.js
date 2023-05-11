8cd2b19ff0719de1e413a401ba53b2a2cfc08cbb
Zachary Beschler <zachmb@gmail.com>
2023-05-10 23:57:26 -0700

    extension code

--- extension.js
+++ extension.js
@@ -4,33 +4,47 @@ const vscode = require('vscode');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "indentify" is now active!');
+	// console.log('Congratulations, your extension "indentify" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('indentify.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from indentify!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerTextEditorCommand('indentify.replace', function(textEditor, edit) {
+		const document = textEditor.document;
+		for(let lineNumber = 0, lineCount = document.lineCount; lineNumber < lineCount; lineNumber++) {
+			const line = document.lineAt(lineNumber).text;
+			edit.replace(
+				new vscode.Range(lineNumber, 0, lineNumber, line.length),
+				indentifyReplace(line)
+			);
+		}
+	}));
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
+function indentifyReplace(line) {
+	let regex = /^\ */;
+	let indent = line.match(regex)[0];
+	console.log( `indent:[` + indent + `]` );
+	let indent_size = Math.ceil(indent.length / 2);
+	let new_indent = "";
+	for(let i = 1; i <= indent_size; i++) {
+		new_indent += "\t";
+	}
+	console.log( `new_indent:[` + new_indent + `]` );
+	let line_replaced = line.replace(regex, new_indent);
+	console.log( `line_replaced:[` + line_replaced + `]` );
+	return line_replaced;
+}
+
 module.exports = {
 	activate,
-	deactivate
-}
+	deactivate,
+	indentifyReplace
+};
